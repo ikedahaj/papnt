6c9c4aef1628f559acfa4eedd2adf22e5c3044ef
ikedahaj <132437214+ikedahaj@users.noreply.github.com>
2024-08-12 23:12:04 +0900

    ・ETにエンターで確定する機能を追加 ・ETのvalue出力を関数に切り出し ・papntを走らせるところを関数化(途中)

--- console_papnt/main.py
+++ console_papnt/main.py
@@ -29,29 +29,56 @@ class Editable_Text(ft.Row):
     def __init__(self,value:str):
         super().__init__()
         self.value=value
-        ET_text=ft.Text(value=value)
+        self.ET_text=ft.Text()
+        self.ET_text.value=value
         ET_button_edit=ft.IconButton(icon=ft.icons.EDIT,on_click=self.ET_clicked_text_edit)
         ET_button_run=ft.IconButton(icon=ft.icons.RUN_CIRCLE,on_click=self.ET_clicked_run_papnt)
         ET_button_delete=ft.IconButton(icon=ft.icons.DELETE,on_click=self.ET_clicked_text_delete)
-        self.controls=[ET_text,ET_button_edit,ET_button_run,ET_button_delete]
+        self.controls=[self.ET_text,ET_button_edit,ET_button_run,ET_button_delete]
     def ET_clicked_text_edit(self,e):
-        self.control_save=self.controls
+        control_save=self.controls
         ET_button_done_edit=ft.FloatingActionButton(icon=ft.icons.DONE,on_click=self.ET_clicked_done_edit)
         new_value=ft.TextField(value=self.value)
+        new_value.on_submit=self.ET_clicked_done_edit
         ET_edit_view=[new_value,ET_button_done_edit]
         self.controls=ET_edit_view
+        self.controls[0].data=control_save
         self.update()
+        new_value.focus()
     def ET_clicked_text_delete(self,e):
         self.clean()
-        pass
+
     def ET_clicked_run_papnt(self,e):
-        papnt.mainfunc.create_records_from_doi(self.value)
-        pass
+        run_papnt_doi(self)
+
     def ET_clicked_done_edit(self,e):
+        saved=self.controls[0].data
         self.value=self.controls[0].value
-        self.controls=self.control_save
-        self.controls[0].value=self.value
+        # saved[0].value=self.controls[0].value
+        saved[0].value=self.controls[0].value
+        self.controls=saved
+        # self.controls[0].value=self.value
         self.update()
+    def update_value(self,value:str):
+        self.value=value
+        self.controls[0].value=value
+        
+
+def run_papnt_doi(now_text:Editable_Text):
+    print("now is doi",now_text.value)
+    doi=now_text.value
+    now_text.update_value("processing...")
+    now_text.update()
+    # print("now is processing")
+    try:
+        papnt.mainfunc.create_records_from_doi(doi)
+    except:
+        exc= sys.exc_info()
+        now_text.update_value("Error: "+str(exc[1]))
+    else:
+        now_text.update_value("Done "+doi)
+    now_text.update()
+    # print("now is done")
 def main(page: ft.Page):
     def add_clicked(e):
         colum.controls.append(Editable_Text(value=new_task.value))
@@ -62,9 +89,8 @@ def main(page: ft.Page):
         colum.clean()
         page.update()
     #Enterキーを押されたら文字を加える.
-    def add_entered(e:ft.KeyboardEvent):
-        if e.key=="Enter":
-            add_clicked(e)
+    def add_entered(e:ft.OptionalEventCallable):
+        add_clicked(e)
     def run_clicked(e):
         database_info=papnt.database.DatabaseInfo()
         client=Client(auth=database_info.tokenkey)
@@ -79,30 +105,17 @@ def main(page: ft.Page):
             input_doi.value=doi
             page.update()
             # continue
-            serch_flag={"filter":{"property":"DOI","rich_text":{"equals":doi}}}
+            serch_flag={"filter":{"property":database_info.propnames["doi"],"rich_text":{"equals":doi}}}
             serch_flag["database_id"]=par["database_id"]
             response=client.databases.query(**serch_flag)
             if len(response["results"])!=0:
-                input_doi.value="Already added! "+doi
+                input_doi.update_value("Already added! "+doi)
                 page.update()
                 continue
-            add_data={"properties":{"DOI":{'rich_text': [{'text': {'content': f'{doi}'}}]}}}
-            add_data["parent"]=par
-            doi_input_result= client.pages.create(**add_data)
-            input_doi.value="processing..."
-            page.update()
-            try:
-                papnt.mainfunc.create_records_from_doi(database,database_info,doi)
-            except:
-                client.pages.update(**{"page_id":doi_input_result["id"],"archived":True})
-                exc= sys.exc_info()
-                input_doi.value="Error: "+str(exc[1])
-            else:
-                input_doi.value="Done "+doi
-            page.update()
+            run_papnt_doi(input_doi)
     # page.title("Papnt Control")
     new_task = ft.TextField(hint_text="Please input DOI")
-    page.on_keyboard_event=add_entered
+    new_task.on_submit=add_entered
     add_bottun=ft.FloatingActionButton(icon=ft.icons.ADD, on_click=add_clicked)
     run_bottun=ft.FloatingActionButton(icon=ft.icons.RUN_CIRCLE, on_click=run_clicked)
     delete_bottun=ft.FloatingActionButton(icon=ft.icons.DELETE, on_click=delete_clicked)
